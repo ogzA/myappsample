afd9b844ac980c6326a67f7a496e04d03536a88d
ogzA <akankn@hotmail.com>
2017-07-22 00:05:14 +0200

    Using Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+
 
 
 /*
@@ -18,8 +23,43 @@ gulp.task('message', function() {
 gulp.task('copyHtml', function() {
     gulp.src('src/*.html')
         .pipe(gulp.dest('dist'));
-})
+});
 
-gulp.task('default', function() {
-    return console.log('Gulp is running');
+// Optimize Images
+gulp.task('imageMin', () =>
+	gulp.src('src/images/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/images'))
+);
+
+// Minify JS
+gulp.task('minify', function() {
+    gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+
+});
+
+// Compile Sass
+gulp.task('sass', function() {
+    gulp.src('src/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));
+});
+
+// Scripts 
+gulp.task('scripts', function() {
+    gulp.src('src/js/*.js')
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', ['message', 'copyHtml', 'imageMin', 'minify', 'sass', 'scripts']); // Tüm Gulp fonksiyonlarını tek bir kod ile çözümlemiş oluyoruz
+
+gulp.task('watch', function() {
+    gulp.watch('src/js/*.js', ['scripts']);
+    gulp.watch('src/images/*', ['imageMin']);
+    gulp.watch('src/sass/*.scss', ['sass']);
+    gulp.watch('src/*.html', ['copyHtml']);
 });
---
+++ src/js/file1.js
@@ -0,0 +1,4 @@
+console.log('This is file 1 ^^');
+
+
+console.log('This is file 2 ^_^');
